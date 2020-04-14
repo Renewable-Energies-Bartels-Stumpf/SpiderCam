b053cf59c32b005cb6b3898c7df25c295d5345d9
Andi Git <andiarbeit1996@gmail.com>
2020-04-14 21:29:55 +0200

    v2

--- Calculation/SpiderCam_Andi.m
+++ Calculation/SpiderCam_Andi.m
@@ -14,13 +14,13 @@ a             = [  0,  0,100]
 b             = [100,  0,100]
 c             = [ 50,100,100]
 #Kreisbewegung definieren
-ende          = 2*pi*10                                             
+ende          = 50#2*pi*10                                             
 p             = 1:ende
 #Kreiskoordinaten errechnen und ablegen
 for i=1:ende   
-   p(i,x)     = 15*cos(i/10)+35                                     
-   p(i,y)     = 15*sin(i/10)+40
-   p(i,z)     = i+30#15*sin(i/10)+30
+   p(i,x)     = i#15*cos(i/10)+35                                     
+   p(i,y)     = 0#15*sin(i/10)+40
+   p(i,z)     = 50#i+30#15*sin(i/10)+30
 end
 #Richtungsvektoren dreieck
 r1            = [c(x)-a(x),c(y)-a(y),c(z)-a(z)]                     
@@ -67,78 +67,78 @@ aktuellerpunkt_y  = [1,delta_B(1)]
 aktuellerpunkt_z  = [1,delta_C(1)]
 
 for i=1:length(p)
-  figure(1);
-  achsenschnitt = (c(y)-c(x)*(c(y)-b(y)/c(x)-b(x)))
-  y1            = (c(y)-a(y)/c(x)-a(x))*p(i,x)
-  y2            = (c(y)-b(y)/c(x)-b(x))*p(i,x) + achsenschnitt
-
-  if(p(i,x)<=c(x)&&p(i,y)<=y1||p(i,x)>c(x)&&p(i,y)<=y2)
-    #Achsendifferenz
-    delta_x     = p(i+1,x)-p(i,x)                                     
-    delta_y     = p(i+1,y)-p(i,y)
-    delta_z     = p(i+1,z)-p(i,z)
-    for k=1:laenge
-      clf
-      #Ecken
-      line([a(x), b(x)        ],[a(y),  b(y)        ],[a(z),  b(z)        ]
-            , "linestyle", "-", "color", "k")
-      line([      b(x), c(x)  ],[       b(y), c(y)  ],[       b(z), c(z)  ]
-            , "linestyle", "-", "color", "k")
-      line([a(x),       c(x)  ],[a(y),        c(y)  ],[a(z),        c(z)  ]
-            , "linestyle", "-", "color", "k")
-      anteil_x  = (delta_x/laenge)*k
-      anteil_y  = (delta_y/laenge)*k
-      anteil_z  = (delta_z/laenge)*k
-      #Plattform
-      line([    p(i,x)+         anteil_x  , p(i,x)+ R1(x)+  anteil_x]
-          ,[    p(i,y)+         anteil_y  , p(i,y)+ R1(y)+  anteil_y]
-          ,[    p(i,z)+         anteil_z  , p(i,z)+         anteil_z]
-          , "linestyle", "-", "color", "k")               
-      line([    p(i,x)+         anteil_x  , p(i,x)+ R2(x)+  anteil_x]
-          ,[    p(i,y)+         anteil_y  , p(i,y)+ R2(y)+  anteil_y]
-          ,[    p(i,z)+         anteil_z  , p(i,z)+         anteil_z]
-          , "linestyle", "-", "color", "k")                                                      
-      line([    p(i,x)+ R1(x)+  anteil_x  , p(i,x)+ R2(x)+  anteil_x]
-          ,[    p(i,y)+ R1(y)+  anteil_y  , p(i,y)+ R2(y)+  anteil_y]
-          ,[    p(i,z)+         anteil_z  , p(i,z)+         anteil_z]
-          , "linestyle", "-", "color", "k") 
-      line([p(i,x)+anteil_x ,p(i,x)+anteil_x],[0                ,p(i,y)+anteil_y],[p(i,z)+anteil_z  ,p(i,z)+anteil_z]
-          , "linestyle", "--", "color", "r")   
-      line([0               ,p(i,x)+anteil_x],[p(i,y)+anteil_y  ,p(i,y)+anteil_y],[p(i,z)+anteil_z  ,p(i,z)+anteil_z]
-          , "linestyle", "--", "color", "g")
-      line([p(i,x)+anteil_x ,p(i,x)+anteil_x],[p(i,y)+anteil_y  ,p(i,y)+anteil_y],[0                ,p(i,z)+anteil_z]
-          , "linestyle", "--", "color", "b")
-      #Seile
-      line([a(x)                          , p(i,x)+         anteil_x]
-          ,[a(y)                          , p(i,y)+         anteil_y]
-          ,[a(z)+               anteil_z  , p(i,z)+         anteil_z]
-          , "linestyle", "-", "color", "r")                                                   
-      line([b(x)                          , p(i,x)+ R2(x)+  anteil_x]
-          ,[b(y)                          , p(i,y)+ R2(y)+  anteil_y]
-          ,[b(z)+               anteil_z  , p(i,z)+         anteil_z]
-          , "linestyle", "-", "color", "g")
-      line([c(x)                          , p(i,x)+ R1(x)+  anteil_x]
-          ,[c(y)                          , p(i,y)+ R1(y)+  anteil_y]
-          ,[c(z)+               anteil_z  , p(i,z)+         anteil_z]
-          , "linestyle", "-", "color", "b") 
-      #Diagramm  
-      view(-10,30);  
-      grid on;
-      rotate3d on;
-      axis([0 100 0 100 0 100]);
-    end
-    line([(100/length(p))*i,(100/length(p))*i],[0,0],[0,100])
-    pause(0.001);
-  elseif
-    clf
-    line([0,100],[0,100],[0,100])
-    line([(100/length(p))*i,(100/length(p))*i],[0,0],[0,100])
-    view(-10,30);  
-    grid on;
-    rotate3d on;
-    axis([0 100 0 100 0 100]);
-    pause(0.001);
-  endif
+##  figure(1);
+##  achsenschnitt = (c(y)-c(x)*(c(y)-b(y)/c(x)-b(x)))
+##  y1            = (c(y)-a(y)/c(x)-a(x))*p(i,x)
+##  y2            = (c(y)-b(y)/c(x)-b(x))*p(i,x) + achsenschnitt
+##
+##  if(p(i,x)<=c(x)&&p(i,y)<=y1||p(i,x)>c(x)&&p(i,y)<=y2)
+##    #Achsendifferenz
+##    delta_x     = p(i+1,x)-p(i,x)                                     
+##    delta_y     = p(i+1,y)-p(i,y)
+##    delta_z     = p(i+1,z)-p(i,z)
+##    for k=1:laenge
+##      clf
+##      #Ecken
+##      line([a(x), b(x)        ],[a(y),  b(y)        ],[a(z),  b(z)        ]
+##            , "linestyle", "-", "color", "k")
+##      line([      b(x), c(x)  ],[       b(y), c(y)  ],[       b(z), c(z)  ]
+##            , "linestyle", "-", "color", "k")
+##      line([a(x),       c(x)  ],[a(y),        c(y)  ],[a(z),        c(z)  ]
+##            , "linestyle", "-", "color", "k")
+##      anteil_x  = (delta_x/laenge)*k
+##      anteil_y  = (delta_y/laenge)*k
+##      anteil_z  = (delta_z/laenge)*k
+##      #Plattform
+##      line([    p(i,x)+         anteil_x  , p(i,x)+ R1(x)+  anteil_x]
+##          ,[    p(i,y)+         anteil_y  , p(i,y)+ R1(y)+  anteil_y]
+##          ,[    p(i,z)+         anteil_z  , p(i,z)+         anteil_z]
+##          , "linestyle", "-", "color", "k")               
+##      line([    p(i,x)+         anteil_x  , p(i,x)+ R2(x)+  anteil_x]
+##          ,[    p(i,y)+         anteil_y  , p(i,y)+ R2(y)+  anteil_y]
+##          ,[    p(i,z)+         anteil_z  , p(i,z)+         anteil_z]
+##          , "linestyle", "-", "color", "k")                                                      
+##      line([    p(i,x)+ R1(x)+  anteil_x  , p(i,x)+ R2(x)+  anteil_x]
+##          ,[    p(i,y)+ R1(y)+  anteil_y  , p(i,y)+ R2(y)+  anteil_y]
+##          ,[    p(i,z)+         anteil_z  , p(i,z)+         anteil_z]
+##          , "linestyle", "-", "color", "k") 
+##      line([p(i,x)+anteil_x ,p(i,x)+anteil_x],[0                ,p(i,y)+anteil_y],[p(i,z)+anteil_z  ,p(i,z)+anteil_z]
+##          , "linestyle", "--", "color", "r")   
+##      line([0               ,p(i,x)+anteil_x],[p(i,y)+anteil_y  ,p(i,y)+anteil_y],[p(i,z)+anteil_z  ,p(i,z)+anteil_z]
+##          , "linestyle", "--", "color", "g")
+##      line([p(i,x)+anteil_x ,p(i,x)+anteil_x],[p(i,y)+anteil_y  ,p(i,y)+anteil_y],[0                ,p(i,z)+anteil_z]
+##          , "linestyle", "--", "color", "b")
+##      #Seile
+##      line([a(x)                          , p(i,x)+         anteil_x]
+##          ,[a(y)                          , p(i,y)+         anteil_y]
+##          ,[a(z)+               anteil_z  , p(i,z)+         anteil_z]
+##          , "linestyle", "-", "color", "r")                                                   
+##      line([b(x)                          , p(i,x)+ R2(x)+  anteil_x]
+##          ,[b(y)                          , p(i,y)+ R2(y)+  anteil_y]
+##          ,[b(z)+               anteil_z  , p(i,z)+         anteil_z]
+##          , "linestyle", "-", "color", "g")
+##      line([c(x)                          , p(i,x)+ R1(x)+  anteil_x]
+##          ,[c(y)                          , p(i,y)+ R1(y)+  anteil_y]
+##          ,[c(z)+               anteil_z  , p(i,z)+         anteil_z]
+##          , "linestyle", "-", "color", "b") 
+##      #Diagramm  
+##      view(-10,30);  
+##      grid on;
+##      rotate3d on;
+##      axis([0 100 0 100 0 100]);
+##    end
+##    line([(100/length(p))*i,(100/length(p))*i],[0,0],[0,100])
+##    pause(0.001);
+##  elseif
+##    clf
+##    line([0,100],[0,100],[0,100])
+##    line([(100/length(p))*i,(100/length(p))*i],[0,0],[0,100])
+##    view(-10,30);  
+##    grid on;
+##    rotate3d on;
+##    axis([0 100 0 100 0 100]);
+##    pause(0.001);
+##  endif
   
   figure(2);
   factor1=30 
@@ -147,12 +147,17 @@ for i=1:length(p)
   line([i,i+1],[p(i,x),p(i+1,x)], "linestyle", "--", "color", "r")
   line([i,i+1],[p(i,y),p(i+1,y)], "linestyle", "--", "color", "g")
   line([i,i+1],[p(i,z),p(i+1,z)], "linestyle", "--", "color", "b")
+  figure(1);
   #Längenänderung
   line([aktuellerpunkt_x(x),i],[aktuellerpunkt_x(y)*factor1,delta_A(i)*factor1], "linestyle", "-", "color", "r")
   line([aktuellerpunkt_y(x),i],[aktuellerpunkt_y(y)*factor1,delta_B(i)*factor1], "linestyle", "-", "color", "g")
   line([aktuellerpunkt_z(x),i],[aktuellerpunkt_z(y)*factor1,delta_C(i)*factor1], "linestyle", "-", "color", "b")
+  h = legend ("delta_A", "delta_B", "delta_C");
   #Nullinie
   line([1,length(p)],[0,0], "linestyle", "-.", "color", "r")
+  grid on;
+  rotate3d on;
+  figure(2);
   #Seillängen
   #line([aktuellelaenge_A,i],[aktuellelaenge_A*factor2,laenge_A(i)*factor2], "linestyle", "-", "color", "r")
   #line([aktuellelaenge_B,i],[aktuellelaenge_B*factor2,laenge_B(i)*factor2], "linestyle", "-", "color", "g")
@@ -166,11 +171,13 @@ for i=1:length(p)
   aktuellelaenge_B  = laenge_B(i)
   aktuellelaenge_C  = laenge_C(i)
   
-  h = legend ("x", "y", "z", "delta_A", "delta_B", "delta_C");
+  h = legend ("x", "y", "z");
   legend (h, "location", "north");
   
   grid on;
   rotate3d on;
   axis([1 length(p) -60 100]);
   pause(0.001);
+  
+  
 end  
